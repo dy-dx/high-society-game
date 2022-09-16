c660a938aa2ef685099a16f45ba4ef5036ebe8ef
Chris Kim <christopherjkim@gmail.com>
2022-09-15 20:30:20 -0400

    gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Client } from 'boardgame.io/react';
+import { Debug } from 'boardgame.io/debug';
 
 import { HighSociety } from './Game';
 import { Board } from './Board';
 
-const App = Client({ game: HighSociety, board: Board, numPlayers: 3 });
+const App = Client({ game: HighSociety, board: Board, numPlayers: 3, debug: { impl: Debug } });
 
 export default App;
